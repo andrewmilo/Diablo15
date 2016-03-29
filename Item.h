1e3728f4a8d2340adf165d9893c29b9418361394
andrewmilo <amiloslavsky@gmail.com>
2016-03-29 15:45:15 -0400

    update item

--- Item.h
+++ Item.h
@@ -1,9 +1,25 @@
 #ifndef _ITEM_
 #define _ITEM_
 
+#include <string>
+#include "Quality.h"
+#include "Type.h"
+
 class Item {
+
+public:
+    Item( std::string name, Quality quality, Type type );
+    std::string get_name( void ) const;
+    void set_name( std::string name );
+    Quality get_quality( void  ) const;
+    void set_quality( Quality quality );
+    Type get_type( void ) const;
+    void set_type( Type type );
     
-    
+private:
+    std::string name;
+    Quality quality;
+    Type type;
 };
 
 #endif
