5d535a8139d7d130418dcf28cbbdefdb19a2a7ae
andrewmilo <amiloslavsky@gmail.com>
2016-03-30 23:16:43 -0400

    small edit

--- Item.cpp
+++ Item.cpp
@@ -8,10 +8,8 @@ Item::Item( std::string name, Quality quality, Type type ){
 }
 
 std::string Item::get_name( void ) const { return name; }
-
 void Item::set_name( std::string name ) { this->name = name; return; }
-
 Quality Item::get_quality( void  ) const { return this->quality; }
-void Item::set_quality( Quality quality ){ this->quality = quality; }
+void Item::set_quality( Quality quality ){ this->quality = quality; return; }
 Type Item::get_type( void ) const { return this->type; }
-void Item::set_type( Type type ) { this->type = type; }
+void Item::set_type( Type type ) { this->type = type; return; }
--- Quality.h
+++ Quality.h
@@ -5,7 +5,6 @@ class Quality {
 
 public:
     enum {
-        
 		White,
 		Gray,
 		Blue,
